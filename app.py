cc7ac2f745988ea3803de7870b0d5371cbaf0d46
ManoJTN <manojtn8618@gmail.com>
2026-07-19 23:38:54 +0530

    Initial project upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+print("====================================")
+print("AI Business Analyst Assistant")
+print("Environment is ready!")
+print("Pandas Version:", pd.__version__)
+print("====================================")
