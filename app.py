d262bd0af550e1ec0a76e4a71b7d626b9b0c6aef
ManoJTN <manojtn8618@gmail.com>
2026-07-29 22:03:47 +0530

    Chart

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ print("Arg count:", ask_business_analyst.__code__.co_argcount)
 from preprocessing.data_loader import load_data
 from preprocessing.profiling import dataset_profile
 
-print("m")
+
 # ----------------------------------------------------
 # Page Configuration
 # ----------------------------------------------------
